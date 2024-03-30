f2b5d63a8d581fd5df1421108d24add060f9de3b
LoFrance <lorenzo.franceschini@pagopa.it>
2024-03-30 19:33:21 +0100

    Add handler to window reaction on darwin os

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,16 @@ const createWindow = () => {
 const main = async () => {
   await app.whenReady();
   createWindow();
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0 ) {
+      createWindow();
+    }
+  })
 };
 
 main();
